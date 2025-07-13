c3b4c74f4ebddad060d49dc1f1e65fc8b187089d
Justinaravinth94 <justin.m@approlabs.com>
2025-07-13 20:40:26 +0530

    deploy code

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,39 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World! justin';
+  getHello(): any {
+    const users = [
+      {
+        id: 1,
+        name: "John Doe",
+        email: "john@example.com",
+        age: 30,
+        isActive: true
+      },
+      {
+        id: 2,
+        name: "Jane Smith",
+        email: "jane@example.com",
+        age: 25,
+        isActive: false
+      },
+      {
+        id: 3,
+        name: "Bob Johnson",
+        email: "bob@example.com",
+        age: 40,
+        isActive: true
+      }
+    ];
+    let updatedArray: any = []
+    users.forEach((item) => {
+      if (item.age === 40) {
+        updatedArray.push({ ...item, age: 9 })
+      } else {
+        updatedArray.push(item)
+      }
+    })
+    return updatedArray;
+
   }
 }
